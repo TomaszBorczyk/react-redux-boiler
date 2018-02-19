78dd5c7e81f78add70c7e87ce90ee0c39ceb24ad
bakowroc <kontakt@maciejbakowicz.pl>
2018-02-19 14:41:17 +0100

    Add editorconfig and logger for redux actions

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -5,7 +5,7 @@ const styles: any = require('./AppContainer.scss');
 export class AppContainer extends React.Component<{}> {
   public render() {
     return (
-      <div className={styles.title}>Hello offtop!</div>
+      <div className={styles.title}>Hello boilerplate!</div>
     );
   }
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,20 @@
 import { isUndefined } from 'lodash';
 import { applyMiddleware, createStore } from 'redux';
+import {createLogger} from 'redux-logger';
 
 import { sagaMiddleware } from '../middleware/saga';
 import reducers from '../reducers';
 
 export default function configure(initialState = {}) {
-
   const createStoreWithMiddleware = applyMiddleware(sagaMiddleware)(createStore);
   const createStoreWithMiddlewareAndRedux = createStoreWithMiddleware as any;
 
-  if (!isUndefined((window as any).__REDUX_DEVTOOLS_EXTENSION__)) {
-    return createStoreWithMiddlewareAndRedux(reducers, initialState, (window as any).__REDUX_DEVTOOLS_EXTENSION__());
+  if (!isUndefined((window as any).__REDUX_DEVTOOLS_EXTENSION__) || process.env.NODE_ENV === 'development') {
+    const logger = createLogger();
+    const createStoreWithMiddlewareWithLogger = applyMiddleware(sagaMiddleware, logger)(createStore);
+    const createStoreWithMiddlewareAndReduxLogger = createStoreWithMiddlewareWithLogger as any;
+
+    return createStoreWithMiddlewareAndReduxLogger(reducers, initialState, (window as any).__REDUX_DEVTOOLS_EXTENSION__());
   }
 
   return createStoreWithMiddlewareAndRedux(reducers, initialState);
