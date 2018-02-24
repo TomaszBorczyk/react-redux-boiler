b76302e5eed28049d3296dc19295c27ff25ec812
bakowroc <kontakt@maciejbakowicz.pl>
2018-02-24 23:35:11 +0100

    Fix store function

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,7 +9,7 @@ export default function configure(initialState = {}) {
   const createStoreWithMiddleware = applyMiddleware(sagaMiddleware)(createStore);
   const createStoreWithMiddlewareAndRedux = createStoreWithMiddleware as any;
 
-  if (!isUndefined((window as any).__REDUX_DEVTOOLS_EXTENSION__) || process.env.NODE_ENV === 'development') {
+  if (!isUndefined((window as any).__REDUX_DEVTOOLS_EXTENSION__) && process.env.NODE_ENV === 'development') {
     const logger = createLogger();
     const createStoreWithMiddlewareWithLogger = applyMiddleware(sagaMiddleware, logger)(createStore);
     const createStoreWithMiddlewareAndReduxLogger = createStoreWithMiddlewareWithLogger as any;
